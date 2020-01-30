e62d0969cfb4251b22000c8f389962a74a8a3292
Lennart Elbe <lenni.elbe@gmail.com>
2020-01-30 10:59:39 +0100

    added ace files to project

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <!DOCTYPE html>
 <HTML>
 <HEAD>
-<TITLE>PRACTICE</TITLE>
+<TITLE>Code Evo</TITLE>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script type="text/javascript" src="./practice.js"></script>
+    <script src="js/ace/ace.js"></script>
 </HEAD>
 
 <BODY>
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm">
-                <h1>Einf. in die Programmierung Klausurlösungen</h1>
-                <textarea rows="4" cols="50" id="input"></textarea>
+                <h1>Code Evo</h1>
             </div>
         </div>
         <div class="row">
+            <div id="program-code"></div>
+        </div>
+        <div class="row">
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Students</button>
+                <div class="dropdown-menu" id="dropdownSwitchEngine" aria-labelledby="dropdownMenuButton">
+                </div>
+            </div>
             <div class="col-sm" style="position: relative;">
                 <div id="program-code">
                 </div>
