2eefc0fa622d4b993ab9b911a1c48e833ec9c976
Lennart Elbe <lenni.elbe@gmail.com>
2020-02-02 18:15:16 +0100

    fixed button css and layout

--- index.php
+++ index.php
@@ -14,25 +14,29 @@
 
 <BODY>
     <div class="container-fluid">
-        <h1 style="margin-left: -15px">Code Evo</h1>
-        <div class="row">
-            <div class="dropdown" style="float: right;">
-                <button class="btn btn-secondary dropdown-toggle" style="margin-left: 10px" type="button" id="studentsDropDown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Students</button>
-                <div class="dropdown-menu" id="dropdownSwitchStudent" aria-labelledby="dropdownMenuButton">
+        <div class="row-fluid">
+            <div class="col-md-12">
+                <h1>Code Evo</h1>
+                <div class="dropdown" style="display: inline-flex;">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="studentsDropDown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Students</button>
+                    <div class="dropdown-menu" id="dropdownSwitchStudent" aria-labelledby="dropdownMenuButton">
+                    </div>
+                </div>
+                <div class="dropdown" style="display: inline-flex;">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="problemsDropDown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Problems</button>
+                    <div class="dropdown-menu" id="dropdownSwitchProblem" aria-labelledby="dropdownMenuButton">
+                    </div>
+                </div>
+                <div class="dropdown" style="display: inline-flex;">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="versionsDropDown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Versions <span class="whichVersion"></span></button>
+                    <div class="dropdown-menu" id="dropdownSwitchVersion" aria-labelledby="dropdownMenuButton">
+                    </div>
+                </div>
+                <div style="margin-left: 8px; display: inline-flex;">
+                    <button type="button" class="btn btn-primary" id="previous" style="margin-right: 4px">&lt;</button>
+                    <button type="button" class="btn btn-primary" id="next">&gt;</button>
                 </div>
             </div>
-            <div class="dropdown" style="float: right;">
-                <button class="btn btn-secondary dropdown-toggle" style="margin-left: 10px" type="button" id="problemsDropDown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Problems</button>
-                <div class="dropdown-menu" id="dropdownSwitchProblem" aria-labelledby="dropdownMenuButton">
-                </div>
-            </div>
-            <div class="dropdown" style="float: right;">
-                <button class="btn btn-secondary dropdown-toggle" style="margin-left: 10px" type="button" id="versionsDropDown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Versions <span class="whichVersion"></span></button>
-                <div class="dropdown-menu" id="dropdownSwitchVersion" aria-labelledby="dropdownMenuButton">
-                </div>
-            </div>
-            <button type="button" class="btn btn-light" id="previous">&lt;</button>
-            <button type="button" class="btn btn-light" id="next">&gt;</button>
         </div>
         <div class="col-md-12">
             <div id="program-code" style="width: 50%; height: 600px"></div>
